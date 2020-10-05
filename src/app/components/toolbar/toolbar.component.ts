fab75a95e6dfca012d541515bb62a2a39cd2fe5a
misha28x <misha1998x@gmail.com>
2020-10-05 11:28:56 +0300

    feat: added toolbar

---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {
+  @Input() title: string;
+}
